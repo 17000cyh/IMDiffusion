e2bba351f0cd25af05735c576df391ccfeaeaa12
Yuhang <v-yuhachen@microsoft.com>
2023-07-05 23:47:21 +0800

    update evaluation

---
+++ compute_score.py
@@ -0,0 +1,272 @@
+import json
+import os
+import pickle
+import torch
+import csv
+from tqdm import tqdm
+import argparse
+import copy
+
+
+def prediction_adjust(prediction, labels):
+    labels = labels[:len(prediction)]
+    i = 0
+    length = len(labels)
+    while i < length:
+        if labels[i] == True:
+            j = i
+
+            adjust_flag = False
+            while labels[j] == True and j < length:
+                if prediction[j] == True:
+                    adjust_flag = True
+                j += 1
+                if j == length:
+                    break
+            if adjust_flag:
+                for k in range(i, j):
+                    prediction[k] = True
+            i = j
+        else:
+            i += 1
+    return prediction
+
+
+def compute_add(prediction, labels):
+    labels = labels[:len(prediction)]
+
+    now_anomaly_flag = False  # 当前点是否是异常点
+    find_anomaly_flag = False  # 当前是否有找到这段异常点
+
+    latency_list = []
+    latency = 0
+
+    for i, label in enumerate(labels):
+        if not label:
+            if now_anomaly_flag:  # 上一个点是异常点
+                latency_list.append(latency)
+                now_anomaly_flag = False
+                find_anomaly_flag = False
+                latency = 0
+            else:
+                pass
+        else:
+            now_anomaly_flag = True
+            if prediction[i]:
+                find_anomaly_flag = True
+
+            if not find_anomaly_flag:
+                latency += 1
+            else:
+                pass
+
+    if latency > 0:
+        latency_list.append(latency)
+
+    return latency_list
+
+
+def compute_f_p_r(prediction, labels):
+    labels = labels[:len(prediction)]
+    TP = torch.sum(prediction * labels)
+    TN = torch.sum((1 - prediction) * (1 - labels))
+    FP = torch.sum(prediction * (1 - labels))
+    FN = torch.sum((1 - prediction) * labels)
+    precise = TP / (TP + FP + 0.00001)
+    recall = TP / (TP + FN + 0.00001)
+
+    f = 2 * precise * recall / (precise + recall + 0.00001)
+    # print(f"f value of {data_id} is {f}, threshold is {threshold}")
+    return f.item(), precise.item(), recall.item()
+
+
+def compute_adjust_f_in_fix_threshold(residual, labels, threshold_proper):
+    threshold = residual.reshape(-1).topk(int(threshold_proper * len(residual))).values[-1].item()
+
+    true = torch.ones_like(residual)
+    false = torch.zeros_like(residual)
+    origin_prediction = torch.where(residual > threshold, true, false)
+
+    adjust_prediction = prediction_adjust(origin_prediction.clone(), labels)
+    adjust_f, adjust_p, adjust_r = compute_f_p_r(adjust_prediction, labels)
+    original_f, original_p, original_r = compute_f_p_r(origin_prediction, labels)
+    add_list = compute_add(origin_prediction, labels)
+    add_value = sum(add_list) / len(add_list)
+
+    return [adjust_f, adjust_p, adjust_r, original_f, original_p, original_r,
+            add_value]
+
+
+def compute_best_threshold_for_average_adjust_f(residual_list, labels):
+    threshold_infor_dict = {}
+    best_f = -1
+    print("begin to search ...")
+    for i in tqdm(range(1, 80)):
+        threshold_proper = i * 0.0005
+        infor_list = []
+        for residual in residual_list:
+            infor_list.append(
+                compute_adjust_f_in_fix_threshold(residual, labels, threshold_proper)
+            )
+            print(f"type of residual is {type(residual)}")
+        threshold_infor_dict[threshold_proper] = infor_list
+
+        # print(infor_list)
+        infor_tensor = torch.Tensor(infor_list)
+        average_infor_tensor = infor_tensor.mean(0)
+        average_adjust_f = average_infor_tensor[0].item()
+        if average_adjust_f > best_f:
+            best_f = average_adjust_f
+            print(f"best f update is {best_f}")
+            best_proper = threshold_proper
+            best_infor = infor_list
+
+    return best_f, best_proper, best_infor, threshold_infor_dict
+
+
+def compute_residual(pk_file, labels, ground_truth, compute_sum, compute_abs):
+    all_gen = pk_file[0]
+
+    all_target = pk_file[1]
+    head = pk_file[5]
+    head_target = pk_file[6]
+
+    head = torch.cat(
+        [head]
+    )
+    head_target = torch.cat(
+        [head_target]
+    )
+
+    all_gen = all_gen[:, :, :]
+    print(f"shape of head is {head.shape}")
+    print(f"shape of all gen is {all_gen.shape}")
+    all_target = all_target[:, :, :]
+    feature_number = all_gen.shape[-1]
+    all_gen = torch.Tensor(all_gen).reshape(-1, feature_number)
+    all_target = torch.Tensor(all_target).reshape(-1, feature_number)
+
+    head = torch.Tensor(head).squeeze()
+    head_target = torch.Tensor(head_target).squeeze()
+
+    all_gen = torch.cat([head, all_gen], dim=0)
+    all_target = torch.cat([head_target, all_target], dim=0)
+    print(f"shape of ground truth is {ground_truth.shape}")
+    print(f"shape of all target is {all_target.shape}")
+    print(f"check equal is {torch.all(all_target == torch.Tensor(ground_truth)[:all_target.shape[0]] * 20)}")
+
+    labels = torch.Tensor(labels)[:len(all_gen)]
+
+    if compute_sum and compute_abs:
+        residual = torch.sum(torch.abs(all_gen - all_target), dim=-1)
+    elif compute_sum and not compute_abs:
+        residual = torch.sum((all_gen - all_target) ** 2, dim=-1)
+    elif not compute_sum and compute_abs:
+        residual, _ = torch.max(torch.abs(all_gen - all_target), dim=-1)
+    elif not compute_sum and not compute_abs:
+        residual, _ = torch.max((all_gen - all_target) ** 2, dim=-1)
+    print(f"shape of residual is {residual.shape}")
+    print(f"shape of labels is {labels.shape}")
+
+    return residual, labels
+
+
+def compute_one_subset_one_strategy(dataset_name, subset_name, compute_sum, compute_abs):
+    residual_list = []
+    for save_file in os.listdir(f"iter_{dataset_name}/window_result"):
+        if "save" in save_file:
+            pass
+        else:
+            continue
+
+        try:
+            os.listdir(f"iter_{dataset_name}/window_result/{save_file}/50")
+        except:
+            continue
+        for subdata_name in os.listdir(f"iter_{dataset_name}/window_result/{save_file}/50"):
+            if subset_name + "_" in subdata_name:
+                pass
+            else:
+                continue
+
+            base_path = f"iter_{dataset_name}/window_result/{save_file}/50/{subdata_name}"
+            for pkl_path in os.listdir(base_path):
+                if ".pk" in pkl_path:
+                    # 记得每次都要读取，否则它会按照地址进行修改
+                    labels = pickle.load(
+                        open(f"iter_{dataset_name}/data/Machine/{subset_name}_test_label.pkl", "rb")
+                    )
+                    ground_truth = pickle.load(
+                        open(f"iter_{dataset_name}/data/Machine/{subset_name}_test.pkl", "rb")
+                    )
+
+                    pkl_file = pickle.load(
+                        open(f"{base_path}/{pkl_path}", 'rb')
+                    )
+                    print("\n\n")
+                    print(f"begin to compute residual for {base_path}")
+                    print(f"pkl file name is {pkl_path}")
+                    print(f"subset name is {subset_name}")
+                    residual, labels = compute_residual(pkl_file, labels=labels, ground_truth=ground_truth,
+                                                        compute_sum=compute_sum, compute_abs=compute_abs)
+                    residual_list.append(residual)
+
+    best_f, best_proper, best_infor, threshold_infor_dict = compute_best_threshold_for_average_adjust_f(
+        residual_list, labels
+    )
+
+    return best_f, best_proper, best_infor, threshold_infor_dict
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", type=str, default="SMD")
+    parser.add_argument("--compute_sum", type=str, default="True")
+    parser.add_argument("--compute_abs", type=str, default="True")
+
+    args = parser.parse_args()
+    dataset_name = args.dataset_name
+    if dataset_name == "SMD":
+        subset_name_list = [f"machine-1-{i}" for i in range(1, 9)]
+        subset_name_list += [f"machine-2-{i}" for i in range(1, 10)]
+        subset_name_list += [f"machine-3-{i}" for i in range(1, 12)]
+    elif dataset_name == "GCP":
+        subset_name_list = [f"service{i}" for i in range(0,30)]
+    else:
+        subset_name_list = [dataset_name]
+
+    os.makedirs(f"score/{dataset_name}/best_infor", exist_ok=True)
+
+    total_infor_csv = csv.writer(
+        open(f"score/{dataset_name}/infor.csv", "a")
+    )
+    if args.compute_sum == "True":
+        args.compute_sum = True
+    elif args.compute_sum == "False":
+        args.compute_sum = False
+    if args.compute_abs == "True":
+        args.compute_abs = True
+    elif args.compute_abs == "False":
+        args.compute_abs = False
+
+    for subset_name in subset_name_list:
+        best_f, best_proper, best_infor, threshold_infor_dict = compute_one_subset_one_strategy(
+            dataset_name, subset_name, args.compute_sum, args.compute_abs
+        )
+        json.dump(
+            best_infor,
+            open(f"score/{dataset_name}/best_infor/{subset_name}_{args.compute_sum}_{args.compute_abs}.json", "w")
+        )
+
+        json.dump(
+            threshold_infor_dict,
+            open(f"score/{dataset_name}/infor_dict/{subset_name}_{args.compute_sum}_{args.compute_abs}.json", "w")
+        )
+        total_infor_csv.writerow(
+            [subset_name, args.compute_sum, args.compute_abs, best_f, best_proper]
+        )
+
+
+
---
+++ ensemble_proper.py
@@ -0,0 +1,466 @@
+import os
+import pickle
+import torch
+import csv
+from tqdm import tqdm
+import argparse
+from pathlib import Path
+
+def prediction_adjust(prediction, labels):
+    labels = labels[:len(prediction)]
+    i = 0
+    length = len(labels)
+    while i < length:
+        if labels[i] == True:
+            j = i
+
+            adjust_flag = False
+            while labels[j] == True and j < length:
+                if prediction[j] == True:
+                    adjust_flag = True
+                j += 1
+                if j == length:
+                    break
+            if adjust_flag:
+                for k in range(i, j):
+                    prediction[k] = True
+            i = j
+        else:
+            i += 1
+    return prediction
+
+def compute_f(prediction,label):
+    label = label[:len(prediction)]
+    TP = torch.sum(prediction * label)
+    TN = torch.sum((1 - prediction) * (1 - label))
+    FP = torch.sum(prediction * (1 - label))
+    FN = torch.sum((1 - prediction) * label)
+    precise = TP / (TP + FP + 0.00001)
+    recall = TP / (TP + FN + 0.00001)
+
+    f = 2 * precise * recall / (precise + recall + 0.00001)
+    return  precise, recall, f
+
+
+def compute_add(prediction, labels):
+    labels = labels[:len(prediction)]
+
+    now_anomaly_flag = False  # 当前点是否是异常点
+    find_anomaly_flag = False  # 当前是否有找到这段异常点
+
+    latency_list = []
+    latency = 0
+
+    for i, label in enumerate(labels):
+        if not label:
+            if now_anomaly_flag:  # 上一个点是异常点
+                latency_list.append(latency)
+                now_anomaly_flag = False
+                find_anomaly_flag = False
+                latency = 0
+            else:
+                pass
+        else:
+            now_anomaly_flag = True
+            if prediction[i]:
+                find_anomaly_flag = True
+
+            if not find_anomaly_flag:
+                latency += 1
+            else:
+                pass
+
+    if latency > 0:
+        latency_list.append(latency)
+
+    return latency_list
+
+def merge(pkl_path,data_id,machine_number = ""):
+    fr = open(pkl_path, "rb")
+    result = pickle.load(fr)
+
+    all_gen = result[0]
+
+    all_target = result[1]
+    head = result[5]
+    head_target = result[6]
+    head_middle = result[-1]  # [50,10,38]
+    all_gen_middle = result[-2]  # [573, 50, 80, 38]
+
+    head = torch.cat(
+        [head]
+    )
+    head_target = torch.cat(
+        [head_target]
+    )
+    # print(f"shape of head middle is {head_middle.shape}")
+    # print(f"shape of all_gen_middle[0, :, 0:15, :] is {all_gen_middle[0, :, 0:15, :].shape}")
+    head_middle = torch.cat(
+        [head_middle], dim=1
+    )
+
+    all_gen = all_gen[:, :, :]
+    all_target = all_target[:, : , :]
+    all_gen_middle = all_gen_middle[:, :,:, :].permute(1, 0, 2, 3)  # [diffusion step, batch number, window length, feature number]
+
+
+    diffusion_steps = all_gen_middle.shape[0]
+    feature_number = all_gen_middle.shape[-1]
+
+    all_gen = torch.Tensor(all_gen).reshape(-1, feature_number)
+    all_target = torch.Tensor(all_target).reshape(-1, feature_number)
+    all_gen_middle = all_gen_middle.reshape(diffusion_steps, -1, feature_number)
+
+
+    head = torch.Tensor(head).squeeze()
+    head_target = torch.Tensor(head_target).squeeze()
+    head_middle = head_middle.squeeze()
+
+    all_gen = torch.cat([head, all_gen], dim=0)
+    all_target = torch.cat([head_target, all_target], dim=0)
+    # print(f'shape of head middle is {head_middle.shape}')
+    # print(f"shape of all gen is {all_gen_middle.shape}")
+    all_gen_middle = torch.cat([head_middle, all_gen_middle], dim=1)
+
+    if data_id == "SMD":
+        print(f"machine number is {machine_number}")
+        label = pickle.load(
+            open(f"data/Machine/{machine_number}_test_label.pkl", "rb")
+        )
+        origin_data = pickle.load(
+            open(f"data/Machine/{machine_number}_test.pkl", "rb")
+        )
+    else:
+        label = pickle.load(
+            open(f"data/Machine/{data_id}_test_label.pkl", "rb")
+        )
+        origin_data = pickle.load(
+            open(f"data/Machine/{data_id}_test.pkl", "rb")
+        )
+
+    print(f"check equal is {torch.all(all_target == torch.Tensor(origin_data)[:all_target.shape[0]] * 20)}")
+
+
+
+    label = torch.Tensor(label)
+    # print(f"all gen shape is {all_gen.shape}")
+
+    # print(f"all gen is middle shape is {all_gen_middle.shape}")
+    # print(f"all target shape is {all_target.shape}")
+    return all_gen_middle, label, all_target
+
+def compute_average_residual(prediction, all_target):
+    residual = torch.sum(
+        (prediction - all_target) ** 2, dim=-1
+    )
+    average_residual = torch.sum(residual) / len(residual)
+
+    return average_residual.item()
+
+def compute_residual(prediction, all_target,compute_abs=True,compute_sum=True):
+    print(f"compute abs is {compute_abs} and compute sum is {compute_sum}")
+    if compute_sum and compute_abs:
+        residual = torch.sum(
+            torch.abs(prediction - all_target), dim=-1
+        )
+    elif compute_sum and not compute_abs:
+        residual = torch.sum(
+            (prediction - all_target) ** 2, dim=-1
+        )
+    elif not compute_sum and compute_abs:
+        residual, _ = torch.max(
+            torch.abs(prediction - all_target), dim=-1
+        )
+    elif not compute_sum and not compute_abs:
+        residual, _ = torch.max(
+            (prediction - all_target) ** 2, dim=-1
+        )
+    return residual
+
+
+
+def ensemble(pkl_path, data_id, ensemble_strategy_list = [],last_step_threshold = 0.02,compute_abs=True,compute_sum=True,machine_number=""):
+    if data_id == "SMD":
+        all_gen_middle, label, all_target = merge(pkl_path, data_id,machine_number=machine_number)
+    else:
+        all_gen_middle, label, all_target = merge(pkl_path,data_id)
+    residual_list = []
+    average_residual_list = []
+    for i in ensemble_strategy_list:
+        residual_list.append(compute_residual(
+            all_gen_middle[i],all_target, compute_abs,compute_sum
+        ))
+        average_residual_list.append(compute_average_residual(
+            # all_gen_middle[i], all_target, compute_abs,compute_sum
+            all_gen_middle[i], all_target
+
+        ))
+    # threshold = residual.reshape(-1).topk(int(0.0005 * threshold * len(residual))).values[-1].item()
+
+    true = torch.ones_like(residual_list[0])
+    false = torch.zeros_like(residual_list[0])
+    origin_prediction = torch.zeros_like(residual_list[0])
+
+    step_prediction_same_proper_list = []
+    step_prediction_anomaly_same_proper_list = []
+
+
+    # origin_prediction = torch.where(residual > threshold, true, false)
+    for i, residual in enumerate(residual_list):
+        # residual_i * proper_i = residual_j * proper_j
+        proper_i = average_residual_list[0] * last_step_threshold/ (average_residual_list[i]) # 选出一个适当的阈值
+        print(f"proper is {proper_i}")
+        proper_number = max(int(proper_i *  len(residual)),1)
+
+        threshold = residual.reshape(-1).topk(proper_number).values[-1].item()
+
+        step_prediction = torch.where(residual > threshold, true, false)
+        if i == 0:
+            first_step_prediction = step_prediction
+
+        same_number = sum(step_prediction == first_step_prediction)
+        same_anomaly_number = sum(step_prediction * first_step_prediction)
+        same_proper = same_number / len(step_prediction)
+        same_anomaly_proper = same_anomaly_number / sum(step_prediction)
+
+        step_prediction_same_proper_list.append(same_proper.item())
+        step_prediction_anomaly_same_proper_list.append(same_anomaly_proper.item())
+
+
+        origin_prediction += step_prediction
+
+    best_f = -1
+    for ensemble_threshold in range(0,len(residual_list)):
+        prediction = torch.where(
+            origin_prediction > ensemble_threshold, true,false
+        )
+        add_list = compute_add(origin_prediction, label)
+        add_value = sum(add_list) / len(add_list)
+
+
+        adjust_prediction = prediction_adjust(prediction,label)
+        p,r,f = compute_f(adjust_prediction,label)
+
+
+
+        if f > best_f:
+            best_f = f
+            best_p = p
+            best_r = r
+            best_add = add_value
+            best_ensemble_threshold = ensemble_threshold
+            print(f"best f update and its value is {best_f.item(),p.item(),r.item()}")
+    return [best_p,best_r,best_f,best_add, best_ensemble_threshold] + [torch.std(torch.Tensor(average_residual_list)).item()] + average_residual_list,\
+           step_prediction_same_proper_list, torch.std(torch.Tensor(step_prediction_same_proper_list)).item(), \
+           step_prediction_anomaly_same_proper_list, torch.std(torch.Tensor(step_prediction_anomaly_same_proper_list)).item()
+
+
+
+def compute_one_strategy(data_id,strategy_name,ensemble_strategy_list,csv_writer,last_step_threshold=0.02):
+    print(f"ensemble for {data_id} in {strategy_name} ...")
+    csv_writer.writerow([data_id,strategy_name])
+    if data_id == "MSL":
+        compute_abs = True
+        compute_sum = True
+    if data_id == "PSM":
+        compute_abs = True
+        compute_sum = False
+    if data_id == "SMAP":
+        compute_abs = True
+        compute_sum = True
+    if data_id == "SWaT":
+        compute_abs = True
+        compute_sum = True
+
+    if data_id == "SMD":
+        compute_abs = True
+        compute_sum = False
+
+    if data_id == "SMD":
+        machine_number_list = [f"machine-1-{i}" for i in range(1, 9)]
+        machine_number_list += [f"machine-2-{i}" for i in range(1,10)]
+        machine_number_list += [f"machine-3-{i}" for i in range(1,12)]
+        # machine_number_list = [f"machine-1-5"]
+        threshold_dict = {}
+        csv_reader = csv.reader(open("score/SMD/infor.csv"))
+        # load threshold dict for each subset
+        for line in csv_reader:
+            if line[1] == 'False' and line[2] == 'True':
+                pass
+            else:
+                continue
+
+            threshold_dict[line[0]] = float(line[-1])
+
+        for machine_number in machine_number_list:
+
+            iter_result_list = []
+            pkl_path_list = []
+            for save_file in os.listdir(f"window_result/"):
+                if "save" not in save_file:
+                    continue
+                for data_file in os.listdir(f"window_result/{save_file}/50/"):
+                    if machine_number +"_" not in data_file or "unconditional" not in data_file:
+                        continue
+                    base_path = f"window_result/{save_file}/50/{data_file}"
+                    # print(base_path)
+                    for pkl_path in os.listdir(base_path):
+                        if ".pk" in pkl_path:
+                            pkl_path_list.append(
+                                f"{base_path}/{pkl_path}"
+                            )
+            print(f'length of pkl path list is {len(pkl_path_list)}')
+            # print(pkl_path_list)
+            for item in pkl_path_list:
+                print(item)
+            last_step_threshold = threshold_dict[machine_number]
+            print(f"now threshold for {machine_number} is {last_step_threshold}")
+            for pkl_path in pkl_path_list:
+                result, same_list, same_std, same_anomaly_list, same_anomaly_std = ensemble(pkl_path, data_id,
+                                                                                            ensemble_strategy_list,
+                                                                                            last_step_threshold,
+                                                                                            compute_abs, compute_sum,machine_number=machine_number)
+                result = list(result)
+                iter_result_list.append(result)
+                # csv_writer.writerow([compute_abs, compute_sum] + result)
+                # csv_writer.writerow(same_list + [same_std])
+                # csv_writer.writerow(same_anomaly_list + [same_anomaly_std])
+                # csv_writer.writerow([])
+            iter_result_tensor = torch.Tensor(iter_result_list)
+            average = iter_result_tensor.mean(0).tolist()
+            f_std = torch.std(iter_result_tensor[:, 0])
+            csv_writer.writerow([f"average for {machine_number}"] + average)
+            # csv_writer.writerow(average)
+            # csv_writer.writerow(['std'])
+            # csv_writer.writerow(
+            #     iter_result_tensor.std(0).tolist()
+            # )
+            # csv_writer.writerow([])
+            # csv_writer.writerow([])
+            # csv_writer.writerow([])
+            # csv_writer.writerow([])
+
+    elif data_id == "GCP":
+        machine_number_list = [f"service{i}" for i in range(0, 30)]
+        threshold_dict = {}
+        csv_reader = csv.reader(open(f"score/{data_id}/infor.csv"))
+        for line in csv_reader:
+            if line[1] == 'False' and line[2] == 'True':
+                pass
+            else:
+                continue
+
+            threshold_dict[line[0]] = float(line[-1])
+
+        for save_file in os.listdir(f"window_result/"):
+            if "save" not in save_file:
+                continue
+
+
+            iter_result_list = []
+            for machine_number in machine_number_list:
+                pkl_path_list = []
+                for data_file in os.listdir(f"window_result/{save_file}/50/"):
+                    if machine_number + "_" not in data_file or "unconditional" not in data_file:
+                        continue
+                    base_path = f"window_result/{save_file}/50/{data_file}"
+                    # print(base_path)
+                    for pkl_path in os.listdir(base_path):
+                        if ".pk" in pkl_path:
+                            pkl_path_list.append(
+                                f"{base_path}/{pkl_path}"
+                            )
+                print(f'length of pkl path list is {len(pkl_path_list)}')
+                last_step_threshold = threshold_dict[machine_number]
+                print(f"now threshold for {machine_number} is {last_step_threshold}")
+                for pkl_path in pkl_path_list:
+                    print(f"now machine number is {machine_number}")
+                    result, same_list, same_std, same_anomaly_list, same_anomaly_std = ensemble(pkl_path, data_id,
+                                                                                                ensemble_strategy_list,
+                                                                                                last_step_threshold,
+                                                                                                compute_abs, compute_sum,
+                                                                                                machine_number=machine_number)
+                    result = list(result)
+
+                    iter_result_list.append(result)
+
+            iter_result_tensor = torch.Tensor(iter_result_list)
+            average = iter_result_tensor.mean(0).tolist()
+            csv_writer.writerow([f"average for {save_file}"] + average)
+
+
+    else:
+
+        iter_result_list = []
+        pkl_path_list = []
+        for save_file in os.listdir(f"window_result/"):
+            if "save" not in save_file:
+                continue
+            for data_file in os.listdir(f"window_result/{save_file}/50/"):
+                if data_id not in data_file or "unconditional" not in data_file:
+                    continue
+                base_path = f"window_result/{save_file}/50/{data_file}"
+                # print(base_path)
+                for pkl_path in os.listdir(base_path):
+                    if ".pk" in pkl_path:
+                        pkl_path_list.append(
+                            f"{base_path}/{pkl_path}"
+                        )
+        print(f'length of pkl path list is {len(pkl_path_list)}')
+        for pkl_path in pkl_path_list:
+            result, same_list, same_std, same_anomaly_list, same_anomaly_std = ensemble(pkl_path,data_id,ensemble_strategy_list,last_step_threshold,compute_abs,compute_sum)
+            result = list(result)
+            iter_result_list.append(result)
+            csv_writer.writerow([compute_abs,compute_sum] + result)
+            csv_writer.writerow(same_list + [same_std])
+            csv_writer.writerow(same_anomaly_list + [same_anomaly_std])
+            csv_writer.writerow([])
+        iter_result_tensor = torch.Tensor(iter_result_list)
+        average = iter_result_tensor.mean(0).tolist()
+        f_std = torch.std(iter_result_tensor[:,0])
+        csv_writer.writerow(['average'])
+        csv_writer.writerow(average )
+        csv_writer.writerow(['std'])
+        csv_writer.writerow(
+            iter_result_tensor.std(0).tolist()
+        )
+        csv_writer.writerow([])
+        csv_writer.writerow([])
+        csv_writer.writerow([])
+        csv_writer.writerow([])
+
+
+def compute_one_data(data_id):
+    strategy_dict = {
+        # "base" : list(range(0,1)),
+        # "10-strategy":list(range(0,10)),
+        # "15-strategy": list(range(0, 15)),
+        # "20-strategy": list(range(0, 20)),
+        # "30-strategy": list(range(0, 30)),
+        # "30-5-skip-strategy": list(range(0, 30,5)),
+        "30-3-skip-strategy": list(range(0, 30, 3)),
+    }
+    os.makedirs("ensemble_residual",exist_ok=True)
+
+    csv_writer = csv.writer(open(f"ensemble_residual/{data_id}.csv","w"))
+    # for ensemble_threshold in range(0,10):
+    for key in strategy_dict.keys():
+        strategy_name = key
+        compute_one_strategy(data_id,strategy_name,
+                             strategy_dict[strategy_name],
+                             csv_writer)
+
+if __name__ =="__main__":
+    # for ensemble_threshold in range(0,10):
+    # compute_one_data("PSM")
+    # compute_one_data("MSL")
+    # compute_one_data("SMD")
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", type=str, default="SMD")
+
+    args = parser.parse_args()
+    compute_one_data(args.dataset_name)
+
+
